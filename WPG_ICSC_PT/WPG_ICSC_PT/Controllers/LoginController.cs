970f558013e3c6afa5dc34d427146811f2c10b39
Mohamed Farah <Mohamed.Farah@icstars-columbus.org>
2018-06-16 21:48:19 -0400

    Create the Login Page

---
+++ WPG_ICSC_PT/WPG_ICSC_PT/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using BOL;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Diagnostics;
+
+namespace WPG_ICSC_PT.Controllers
+{
+    public class LoginController : Controller
+    {
+        EmployeeSecurity empSecurty = new EmployeeSecurity();
+
+
+        // GET: /Security/Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn(Employee employee)
+        {
+            try
+            {
+                var isUserValid = empSecurty.Login(employee.Email, employee.Password);
+                if (isUserValid)
+                {
+                    ViewBag.Name = employee.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["Msg"] = "Login Failed ";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception E1)
+            {
+                TempData["Msg"] = "Login Failed " + E1.Message;
+                return RedirectToAction("Index");
+            }
+        }        
+    }
+}
