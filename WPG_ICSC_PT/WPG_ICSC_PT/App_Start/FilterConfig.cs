e2e5208b42fe28955e40d6682e056f751df0e3b9
Mohamed Farah <Mohamed.Farah@icstars-columbus.org>
2018-06-19 14:08:09 +0000

    Revert "Merged PR 4: Login Security Completed
    Login, Logout, and other security features.
    Home page tweaked a little bit. It will be completed soon."

--- WPG_ICSC_PT/WPG_ICSC_PT/App_Start/FilterConfig.cs
+++ WPG_ICSC_PT/WPG_ICSC_PT/App_Start/FilterConfig.cs
@@ -8,7 +8,6 @@ namespace WPG_ICSC_PT
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
         }
     }
 }
---
+++ WPG_ICSC_PT/WPG_ICSC_PT/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using BOL;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Diagnostics;
+using System.Web.Routing;
+
+namespace WPG_ICSC_PT.Controllers
+{
+    public class LoginController : Controller
+    {
+        EmployeeSecurity empSecurty = new EmployeeSecurity();
+        EmployeeBL empBL = new EmployeeBL();
+
+
+        // GET: /Security/Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult SignIn(Employee employee)
+        {
+            try
+            {
+                var currentEmp = empBL.GetALL().FirstOrDefault(x => x.Email == employee.Email && x.Password == employee.Password);
+                var isUserValid = empSecurty.Login(employee.Email, employee.Password);
+                if (isUserValid)
+                {
+                    ViewBag.Name = employee.Email;
+                    //return RedirectToAction("Index", "Home", new { employee.Id});
+                    return RedirectToAction("Index", new RouteValueDictionary(
+                        new { Controller = "Home", Action = "Index", Id = currentEmp.Id }));
+                }
+                else
+                {
+                    TempData["Msg"] = "Login Failed ";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception E1)
+            {
+                TempData["Msg"] = "Login Failed " + E1.Message;
+                return RedirectToAction("Index");
+            }
+        }        
+    }
+}
