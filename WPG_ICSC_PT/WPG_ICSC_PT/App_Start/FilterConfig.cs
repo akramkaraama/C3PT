44b0e082fa196f4221eff19d843a8a6e9e7dda1a
Mohamed Farah <mohamed.farah@icstars-columbus.org>
2018-06-19 01:31:06 -0400

    1: Completed the Login page functionality (Added Logout), and all it's security features 2: Tweaked the home page a little bit (closer to the target now)...!!!

--- WPG_ICSC_PT/WPG_ICSC_PT/App_Start/FilterConfig.cs
+++ WPG_ICSC_PT/WPG_ICSC_PT/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace WPG_ICSC_PT
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
